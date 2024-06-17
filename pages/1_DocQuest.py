3702df586eb8077d7024aca9e517faaa5de1d66f
Yuvraj Singh <ys2002github@gmail.com>
2024-06-17 09:29:12 +0530

    Added the functions for creating chunks of document and loading the document

---
+++ pages/1_DocQuest.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import langchain
+import pinecone
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain.llms import OpenAI
+from sentence_transformers import SentenceTransformer
+
+
+@st.cache_resource
+def load_embedding_model():
+    emb_model = SentenceTransformer("all-MiniLM-L6-v2")
+    return emb_model
+
+
+
+def chunk_data(docs, chunk_size=800, chunk_overlap=50):
+    """
+    Chunk large documents into smaller segments.
+
+    Parameters:
+    - docs (str or list): Input documents to be chunked. If a single string, it will be treated as one document.
+                          If a list of strings, each element will be treated as a separate document.
+    - chunk_size (int): Size of each chunk in characters (default is 800).
+    - chunk_overlap (int): Number of characters to overlap between adjacent chunks (default is 50).
+
+    Returns:
+    - list: List of chunked documents, where each element represents a chunk.
+    """
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    chunked_docs = text_splitter.split_documents(docs)
+    return chunked_docs
+
+def Doc_QA():
+    st.title("Doc Quest")
+    pdf_file = st.file_uploader("Upload the query document", type="pdf")
+
+    if pdf_file is not None:
+
+        # Saving the uploaded PDF file
+        with open("uploaded_pdf.pdf", "wb") as f:
+            f.write(pdf_file.read())
+
+        # Use PyPDFLoader to load and split the PDF
+        loader = PyPDFLoader("uploaded_pdf.pdf")
+        pages = loader.load_and_split()
+
+        st.write(len(pages))
+
+        # Calling the function to create chunks
+        chunks = chunk_data(docs=pages)
+        st.write(len(chunks))
+
+
+Doc_QA()
