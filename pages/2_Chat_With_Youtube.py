f2a7f9dddb84414393f78461b4e99cf0ee073a19
Yuvraj Singh <ys2002github@gmail.com>
2024-06-19 13:28:24 +0530

    Added the function for loading llm ,embedding model and also the whisper model. At the same timer I have also added the function for getting the viddeo transcription from the Youtube video URL

--- pages/2_Chat_With_Youtube.py
+++ pages/2_Chat_With_Youtube.py
@@ -1,10 +1,20 @@
 import streamlit as st
+import tempfile
+import whisper
+from pytube import YouTube
+from langchain_community.llms import Ollama
+from langchain_community.embeddings import OllamaEmbeddings
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
 
 st.markdown(
     """
         <style>
                .block-container {
-                    padding-top: 0.5rem;
+                    padding-top: 0.7rem;
                     padding-bottom: 0rem;
                     # padding-left: 2rem;
                     # padding-right:2rem;
@@ -13,7 +23,6 @@ st.markdown(
         """,
     unsafe_allow_html=True,
 )
-
 
 
 # Function to extract the video ID from the YouTube URL
@@ -25,29 +34,57 @@ def get_youtube_id(url):
     return match.group(6) if match else None
 
 
+@st.cache_resource
+def load_models():
+    model = Ollama(model="llama3")
+    embedding_model = OllamaEmbeddings(model="jina/jina-embeddings-v2-base-de")
+    whisper_model = whisper.load_model("base")
+    return [whisper_model,model,embedding_model]
+
+
+def get_video_transcript(whisper_model, youtube_url):
+
+    transcription = ""
+    youtube = YouTube(youtube_url)
+    audio = youtube.streams.filter(only_audio=True).first()
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        file = audio.download(output_path=tmpdir)
+        transcription = whisper_model.transcribe(file, fp16=False)["text"].strip()
+
+        # Always write the transcription to the file, overwriting if it exists
+        with open("./video_content.txt", "w") as file:
+            file.write(transcription)
+
+    return transcription
+
 
 def chat_with_utube():
 
     st.markdown(
-        "<h1 style='text-align: center; font-size: 55px;'>Chat with Youtube</h1>",
+        "<h1 style='text-align: left; font-size: 50px;'>Chat with Youtube</h1>",
         unsafe_allow_html=True,
     )
-    st.markdown(
-            "<p style='font-size: 22px; text-align: center;padding-left: 2rem;padding-right: 2rem;'>In times of tough market situations, fake job postings and scams often spike, posing a significant threat to job seekers. To combat this, I've developed a user-friendly module designed to protect individuals from falling prey to such fraudulent activities. This module requires users to input details about the job posting they're considering. Behind the scenes, two powerful AI models thoroughly analyze the provided information. Once completed, users receive a clear indication of whether the job posting is genuine or potentially deceptive.</p>",
-            unsafe_allow_html=True,
-        )
-    youtube_link = st.text_input("Enter the Youtube video URL")
-   
 
-    col1,col2 = st.columns(spec=(0.7,1), gap="large")
-    chat_button = False
+    # Calling the function to load the Whisper model, LLM and embedding model
+    whisper,llm,embedding_model = load_models()
+    
+
+    col1,col2 = st.columns(spec=(1.3,1), gap="large")
+    chat_bt = False
+    transcrip_bt = False
 
     with col1:
+        st.markdown(
+            "<p style='font-size: 20px; text-align: left;'>In times of tough market situations, fake job postings and scams often spike, posing a significant threat to job seekers. To combat this, I've developed a user-friendly module designed to protect individuals from falling prey to such fraudulent activities. This module requires users to input details about the job posting they're considering. Behind the scenes, two  provided information. Once completed, users receive a clear indication of whether the job posting is genuine or potentially deceptive.</p>",
+            unsafe_allow_html=True,
+        )
+        youtube_link = st.text_input("Enter the Youtube video URL")
         if youtube_link:
             video_id = get_youtube_id(youtube_link)
             if video_id:
                 st.markdown(f"""
-                    <iframe width="700" height="394" src="https://www.youtube.com/embed/{video_id}" 
+                    <iframe width="950" height="420" src="https://www.youtube.com/embed/{video_id}" 
                     frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                     allowfullscreen></iframe>
                 """, unsafe_allow_html=True)
@@ -55,15 +92,22 @@ def chat_with_utube():
                 st.error("Invalid YouTube URL")
 
         st.write("")
-        with st.container(border=True):
-            transcrip_col,chat_col = st.columns(spec=(1,1), gap="large")
-            with transcrip_col:
-                transcrip_bt = st.button("Get transcript",use_container_width=True)
-            with chat_col:
-                chat_bt = st.button("Start Chat",use_container_width=True)
+        transcrip_col,chat_col = st.columns(spec=(1,1), gap="large")
+        with transcrip_col:
+            transcrip_bt = st.button("Get transcript",use_container_width=True)
+            if transcrip_bt:
+                transcription = get_video_transcript(whisper,youtube_link)
+                with chat_col:
+                    st.success("Video transcription extracted succesfully")
+                    st.write(transcription)
+
+        with chat_col:
+            chat_bt = st.button("Start Chat",use_container_width=True)
+            
             
     with col2:
-        if chat_button:
+        if transcrip_bt:
             pass
+            
 
 chat_with_utube()
