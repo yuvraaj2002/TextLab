99b748eda7a1e27ea0f6e239c35f59ed26469a44
Yuvraj Singh <ys2002github@gmail.com>
2024-06-21 19:24:31 +0530

    Removed the extra code for setting up vector database for every run and also updated the UI

--- pages/1_DocQuest.py
+++ pages/1_DocQuest.py
@@ -1,16 +1,16 @@
 import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
 from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
 
 from langchain_pinecone import PineconeVectorStore
-from pinecone.grpc import PineconeGRPC as Pinecone
-from pinecone import ServerlessSpec
+# from pinecone.grpc import PineconeGRPC as Pinecone
+# from pinecone import ServerlessSpec
 
 from langchain.schema.output_parser import StrOutputParser
 from langchain.prompts import PromptTemplate
-
 from PyPDF2 import PdfReader
 import os
 import io
@@ -35,30 +35,14 @@ st.markdown(
 
 
 
+
 @st.cache_resource
 def load_models():
-    model = Ollama(model="llama3")
+
     embedding_model = OllamaEmbeddings(model="jina/jina-embeddings-v2-base-de")
+    llm = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=os.environ['GEMINI_API'])
     
-    # Setup for Pinecone vector database
-    pc = Pinecone(api_key=os.environ['PINECONE_API_KEY'])
-    index_name = "docquest"
-
-    if index_name in pc.list_indexes().names():
-        pc.delete_index(index_name)
-    else:
-        # Creating a new index
-        pc.create_index(
-            name=index_name,
-            dimension=768,
-            metric="cosine",
-            spec=ServerlessSpec(
-                cloud='aws',
-                region='us-east-1'
-            )
-        )
-
-    return (model,embedding_model)
+    return embedding_model,llm
 
 
 
@@ -85,19 +69,24 @@ def split_load_data(pdf_data,index_name,embedding_model):
 
 
 
-def get_answer(context_text, query_text, llm):
+def get_answer(vdb_contxt_text, query_text, llm):
 
     template = f"""
-        Given the query '{query_text}', and after reviewing the information retrieved from the vector database:
-        {context_text}
+        Given the query '{{query_text}}', and after reviewing the information retrieved from the vector database:
+        {{vdb_contxt_text}}
         Please provide a concise and informative answer that addresses the query effectively.
     """
 
-    prompt = PromptTemplate(input_variables=["query_text", "context_text"], template=template)
+    # Define the input variable names
+    input_variables = ["query_text", "vdb_contxt_text"]
+
+    # Create the prompt template
+    prompt = PromptTemplate(input_variables=input_variables, template=template)
     output_parser = StrOutputParser()
     chain = prompt | llm | output_parser
 
-    response = chain.invoke({"context_text": context_text, "query_text": query_text})
+    # Pass the actual values for the variables
+    response = chain.invoke({"query_text": query_text, "vdb_contxt_text": vdb_contxt_text})
     return response
 
 
@@ -108,15 +97,15 @@ def Doc_QA():
      
 
     # Calling the function to load the Whisper model, LLM and embedding model
-    llm,embedding_model = load_models()
+    embedding_model,llm = load_models()
 
     col1,col2 = st.columns(spec=(1.5,1), gap="large")
     with col1:
         st.markdown(
-        "<h1 style='text-align: left; font-size: 50px;'>Chat with Documents</h1>",
+        "<h1 style='text-align: left; font-size: 50px;'>Chat with Documents📃</h1>",
         unsafe_allow_html=True)
         st.markdown(
-            "<p style='font-size: 20px; text-align: left;'>In times of tough market situations, fake job postings and scams often spike, posing a significant threat to job seekers. To combat this, I've developed a user-friendly module designed to protect individuals from falling prey to such fraudulent activities. This module requires users to input details about the job posting they're considering. Behind the sceeptive.</p>",
+            "<p style='font-size: 20px; text-align: left;'>Welcome to our advanced PDF Query Module, where you can harness the power of Large Language Models (LLMs) and vector databases to efficiently query your PDF documents. This module simplifies the process of interacting with your PDF files, making information retrieval both quick and intuitive.Experience the ease of querying and extracting information from your PDF documents like never before, powered by cutting-edge AI and database technology..</p>",
             unsafe_allow_html=True,
         )
         pdf_file = st.file_uploader("Upload the query document", type="pdf")
@@ -128,20 +117,28 @@ def Doc_QA():
             st.write("")
             pdf_data = pdf_file.read()
             b64_pdf = base64.b64encode(pdf_data).decode("utf-8")
-            pdf_display = f'<embed src="data:application/pdf;base64,{b64_pdf}" width="690" height="740" type="application/pdf">'
+            pdf_display = f'<embed src="data:application/pdf;base64,{b64_pdf}" width="690" height="750" type="application/pdf">'
             st.markdown(pdf_display, unsafe_allow_html=True)
-        with col1:
 
+        with col1:
             # Calling the function to extract the pdf data and create chunks
             pinecone = split_load_data(pdf_data,"docquest",embedding_model)
-            query_text = st.text_input("Enter your query : ")
-            if query_text:
-                result = pinecone.similarity_search(query_text)[:1]
-                context_text = result[0].page_content
+            response = None
+            with st.container(border=True):
+                query_text = st.text_input("Enter your query : ")
+                if query_text:
+                    result = pinecone.similarity_search(query_text)[:1]
+                    vdb_context_text = result[0].page_content
 
-                # Calling the function to get the answer from the LLM
-                response = get_answer(context_text,query_text,llm)
-                st.write(response)
+                    # Calling the function to get the answer from the LLM
+                    response = get_answer(vdb_context_text,query_text,llm)
+
+            if response is not None:
+                with st.container(border=True):
+                    st.markdown(
+                        f"<p style='font-size: 20px;'>{response}</p>",
+                        unsafe_allow_html=True
+                    )
 
 
 
